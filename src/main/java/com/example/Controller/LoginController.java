614c71ecf0dfef10a00ddc316077b3a024c81395
coustea <3329368499@qq.com>
2025-04-12 18:09:31 +0800

    增加跨域问题的配置

---
+++ src/main/java/com/example/Controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.example.Controller;
+
+import com.example.entity.ApiResult;
+import com.example.entity.Student;
+import com.example.service.impl.StudentServiceImpl;
+import com.example.utils.ApiResultHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+    @Autowired
+    private StudentServiceImpl studentService;
+
+    @PostMapping
+    public ApiResult login(@RequestBody Student student){
+
+        if (student == null){
+            return ApiResultHandler.buildApiResult(400,"请求失败",null);
+        }
+        Student student1 = studentService.login(student);
+        if (student1 == null){
+            return ApiResultHandler.buildApiResult(400,"请求失败",null);
+        }
+        else {
+            return ApiResultHandler.buildApiResult(200,"请求成功",student1);
+        }
+    }
+
+}
