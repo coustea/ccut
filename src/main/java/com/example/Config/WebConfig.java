56018c0bf549d68ad6746d2f2f8075aef46d28c7
coustea <3329368499@qq.com>
2025-04-12 16:39:57 +0800

    增加跨域问题的配置

---
+++ src/main/java/com/example/Config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.Config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final long MAX_AGE = 24 * 60 * 60;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(MAX_AGE);
+    }
+}
