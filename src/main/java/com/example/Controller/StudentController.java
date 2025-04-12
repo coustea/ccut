e07598428eecbdf1f2aaa295fd30282dc7e0d08f
coustea <3329368499@qq.com>
2025-04-12 18:17:16 +0800

    添加了表现层的代码

--- src/main/java/com/example/Controller/StudentController.java
+++ src/main/java/com/example/Controller/StudentController.java
@@ -1,16 +1,30 @@
 package com.example.Controller;
 
+import com.example.entity.ApiResult;
 import com.example.entity.Student;
+import com.example.service.impl.StudentServiceImpl;
+import com.example.utils.ApiResultHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
+    @Autowired
+    private StudentServiceImpl studentService;
+
+    @PutMapping("/{id}")
+    public ApiResult updateStudent(@PathVariable Integer id,@RequestBody Student student){
+        if (student == null){
+            return ApiResultHandler.buildApiResult(400,"请求失败",null);
+        }
+        else {
+            studentService.updateStudent(student);
+            return ApiResultHandler.buildApiResult(200,"请求成功",student);
+        }
+    }
 
 
 
